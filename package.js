9ce18decafdbf2efa5228b5b3d25fbaed8b7ac56
tosbourn <toby.osbourn@googlemail.com>
2015-07-11 11:04:45 +0100

    Updates package

--- package.js
+++ package.js
@@ -1,18 +1,18 @@
 Package.describe({
-  name: 'overture8:dynamic-inputs',
-  summary: ' /* Fill me in! */ ',
+  name: 'tosbourn:dynamic-inputs',
+  summary: 'Creates dynamic inputs for use in forms',
   version: '1.0.0',
-  git: ' /* Fill me in! */ '
+  git: 'git://github.com/tosbourn/dynamic-inputs'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
-  api.use(['jquery', 'tracker', 'templating'], 'client');
-  api.addFiles(['overture8:dynamic-inputs.html', 'overture8:dynamic-inputs.js'], 'client');
+  api.use(['jquery', 'tracker', 'templating'], '/client');
+  api.addFiles(['tosbourn:dynamic-inputs.html', 'tosbourn:dynamic-inputs.js'], 'client');
 });
 
 Package.onTest(function(api) {
   api.use('tinytest');
-  api.use('overture8:dynamic-inputs');
-  api.addFiles('overture8:dynamic-inputs-tests.js');
+  api.use('tosbourn:dynamic-inputs');
+  api.addFiles('tosbourn:dynamic-inputs-tests.js');
 });
