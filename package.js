c4dd09ba92573e79beb6402b479b18d8e801139c
tosbourn <toby.osbourn@googlemail.com>
2015-07-11 11:12:45 +0100

    Don't namespace files

--- package.js
+++ package.js
@@ -8,11 +8,11 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
   api.use(['jquery', 'tracker', 'templating'], 'client');
-  api.addFiles(['tosbourn:dynamic-inputs.html', 'tosbourn:dynamic-inputs.js'], 'client');
+  api.addFiles(['dynamic-inputs.html', 'dynamic-inputs.js'], 'client');
 });
 
 Package.onTest(function(api) {
   api.use('tinytest');
   api.use('tosbourn:dynamic-inputs');
-  api.addFiles('tosbourn:dynamic-inputs-tests.js');
+  api.addFiles('dynamic-inputs-tests.js');
 });
